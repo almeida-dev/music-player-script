916e1af680cfffeeecb00410ccb3776e1768b4d4
almeida-rust <almeida.rustc@gmail.com>
2022-11-29 20:21:35 -0300

    edition 1.0

---
+++ window.rs
@@ -0,0 +1,22 @@
+// to install gtk3 in debian op distributions:
+// sudo apt-get install libgtk-3-dev
+
+extern crate gio;
+extern crate gtk;
+
+use std::env;
+
+use gio::{ApplicationExt, ApplicationExtManual, ApplicationFlags};
+use gtk::{Application, ApplicationWindow, GtkWindowExt, WidgetExt};
+
+fn main() {
+    let application = Application::new("com.github.rust-by-example", ApplicationFlags::empty())
+        .expect("Application initialization failed");
+    application.connect_startup(|application| {
+        let window = ApplicationWindow::new(&application);
+        window.set_title("MARIOPLAY7");
+        window.show();
+    });
+    application.connect_activate(|_| {});
+    application.run(&env::args().collect::<Vec<_>>());
+}
